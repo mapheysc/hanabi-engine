98da7e4068b5cd08974c6000a5ce2b30d32ae6a5
Maphey <maphey1@slabbulkhead.llnl.gov>
2019-08-30 19:44:53 -0700

    added license and setup for pypi

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 
 from setuptools import setup, find_packages
 
-VERSION = __import__('hanabi').__VERSION__
+VERSION = '1.0.0'
 
-setup(name='hanabi',
+setup(name='hanaby',
       version=VERSION,
       author='Sam Maphey',
       packages=find_packages(),
       install_requires=[
           'addict',
-      ]
+      ],
+      download_url='https://github.com/mapheysc/hanabi-engine/archive/1.0.0.tar.gz'
       )
